edc6c99e9f0ddb574939162e24c16a04509e4a03
Monomadic <deathdisco@protonmail.com>
2021-09-07 20:31:44 +0800

    added vercel adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,42 +1,44 @@
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [preprocess({
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: [preprocess({
         "postcss": true
     })],
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	},
-	// vite: {
-	// 	ssr: {
-	// 		external: [
-	// 			/@babel\/runtime/,
-	// 			'bn.js',
-	// 			'borsh',
-	// 			'bs58',
-	// 			'buffer-layout',
-	// 			'crypto-hash',
-	// 			'jayson/lib/client/browser',
-	// 			'js-sha3',
-	// 			'node-fetch',
-	// 			'rpc-websockets',
-	// 			'secp256k1',
-	// 			'superstruct',
-	// 			'tweetnacl'
-	// 		]
-	// 	},
-	// 	resolve: {
-	// 		mainFields: ['browser', 'module', 'main', 'jsnext'],
-	// 		dedupe: ['bn.js', 'buffer'],
-	// 		extensions: ['.js', '.ts'],
-	// 		preferBuiltins: false
-	// 	}
-	// }
+    kit: {
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: '#svelte',
+        adapter: vercel()
+    },
+    // vite: {
+    // 	ssr: {
+    // 		external: [
+    // 			/@babel\/runtime/,
+    // 			'bn.js',
+    // 			'borsh',
+    // 			'bs58',
+    // 			'buffer-layout',
+    // 			'crypto-hash',
+    // 			'jayson/lib/client/browser',
+    // 			'js-sha3',
+    // 			'node-fetch',
+    // 			'rpc-websockets',
+    // 			'secp256k1',
+    // 			'superstruct',
+    // 			'tweetnacl'
+    // 		]
+    // 	},
+    // 	resolve: {
+    // 		mainFields: ['browser', 'module', 'main', 'jsnext'],
+    // 		dedupe: ['bn.js', 'buffer'],
+    // 		extensions: ['.js', '.ts'],
+    // 		preferBuiltins: false
+    // 	}
+    // }
 };
 
 export default config;
