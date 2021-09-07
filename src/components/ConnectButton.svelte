22d8f7dad491426208e27a1ca990884da6144046
Monomadic <deathdisco@protonmail.com>
2021-09-07 20:16:03 +0800

    styling for token list

--- src/components/ConnectButton.svelte
+++ src/components/ConnectButton.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
-	import { pubKey } from '../stores/signer';
+	import { pubKey, pubKey } from '../stores/signer';
 	import { connect, disconnect } from '../lib/wallet';
+
+	$: shortAddress = $pubKey
+		? `${$pubKey.substring(0, 5)}...${$pubKey.substring($pubKey.length - 4)}`
+		: null;
 </script>
 
 <main>
 	{#if $pubKey}
-		<div class="btn" on:click={disconnect}>Disconnect</div>
+		<div class="btn btn-primary" on:click={disconnect}>{shortAddress}</div>
 	{:else}
-		<div class="btn" on:click={connect}>Connect</div>
+		<div class="btn btn-primary" on:click={connect}>Connect</div>
 	{/if}
 </main>
--- src/components/SolBalance.svelte
+++ src/components/SolBalance.svelte
@@ -6,17 +6,23 @@
 </script>
 
 <main>
-	<p>
-		SOL Balance:
-		{#await balance}
-			...
-		{:then _balance}
-			{(_balance / 1e9).toLocaleString('en', {
-				minimumFractionDigits: 2,
-				maximumFractionDigits: 2
-			})}
-		{:catch err}
-			{err.message}
-		{/await}
-	</p>
+	<div class="flex space-x-1 my-3 justify-end max-w-screen-sm">
+		<img
+			src="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png"
+			class="rounded-full h-6"
+		/>
+		<div>
+			SOL
+			{#await balance}
+				...
+			{:then _balance}
+				{(_balance / 1e9).toLocaleString('en', {
+					minimumFractionDigits: 2,
+					maximumFractionDigits: 2
+				})}
+			{:catch err}
+				{err.message}
+			{/await}
+		</div>
+	</div>
 </main>
